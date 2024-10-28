e0f5b1fba83aabe7fc4de3107e13a94d0dc14e31
Admin <jedatoos@gmail.com>
2024-10-28 15:07:58 -0500

    mensaje

--- src/main/java/com/betek/gym/gymApplication.java
+++ src/main/java/com/betek/gym/gymApplication.java
@@ -3,6 +3,10 @@ package com.betek.gym;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -11,5 +15,19 @@ public class gymApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(gymApplication.class, args);
 	}
-
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
+
+
